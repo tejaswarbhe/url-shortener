5b260737b7a1fcf881697d987bbd41b2853daf0b
Tejas Warbhe <119755857+tejaswarbhe@users.noreply.github.com>
2025-08-16 00:58:34 +0530

    NEw fix for deployment

--- backend/client/vite.config.js
+++ backend/client/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    minify: true, // Use default minifier (esbuild)
     rollupOptions: {
       output: {
         manualChunks: {
